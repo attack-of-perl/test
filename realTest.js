e247fce57220bcc25a390b792fd5969483e21d6f
Zach Perlman <zach.perlman@wustl.edu>
2018-04-20 15:45:43 -0500

    almost done

--- realTest.js
+++ realTest.js
@@ -2,7 +2,7 @@ var webdriver = require ('selenium-webdriver'),
 By = webdriver.By,
 until = webdriver.until;
 const time = 5000;
-
+var runNum = 11; //MUST INCREMENT BEFORE RUNNING AGAIN
 var driver = new webdriver.Builder().forBrowser('chrome').build();
 
 
@@ -13,22 +13,41 @@ driver.wait(until.elementLocated(By.id("join-hive")),time).then(driver.findEleme
 
 
 //driver.wait(until.elementLocated(By.className("hv btn btn-primary js-ob-video-continue")),time).then(click());
-driver.sleep(4000);
+driver.sleep(3000);
 driver.findElement(By.className("hv btn btn-primary js-ob-video-continue")).click();
 
 //driver.wait(until.elementIsEnabled(By.id("firstName")),time).then(fill);
-driver.sleep(3000).then(fill);
-function fill(){
+driver.sleep(2000).then(fill);
+function fill(){    //fills in fields
     driver.findElement(By.id("firstName")).sendKeys("John");
     driver.findElement(By.id("lastName")).sendKeys("Doe");
     driver.findElement(By.id("phone")).sendKeys("3051231234");
-    driver.findElement(By.id("email")).sendKeys("user@example.com");
+    driver.findElement(By.id("email")).sendKeys("user"+runNum+"@example.com");
     driver.findElement(By.id("password")).sendKeys("abcdef123456");
 }
 
-driver.sleep(500).then(driver.findElement(By.className("hv btn btn-primary ladda-button")).click());
+driver.sleep(500);
+driver.findElement(By.className("hv btn btn-primary ladda-button")).click();
 
+//now fill in organization name
+driver.sleep(2000);
+driver.findElement(By.className("js-workspace-input input__field")).sendKeys("My Organization");
 
+//continue
+driver.sleep(1000);
+driver.findElement(By.className("hv btn btn-primary js-complete-step ladda-button")).click();
+
+//continue
+driver.sleep(4000);
+driver.findElement(By.className("hv btn btn-primary js-complete-step")).click();
+
+//skip
+driver.sleep(1000);
+driver.findElement(By.className("hv btn btn-default js-skip-step")).click();
+
+//skip
+driver.sleep(1000);
+driver.findElement(By.className("hv btn btn-default js-complete-step")).click().then(console.log("successful sign up"));
 
 console.log("made it!")
 
